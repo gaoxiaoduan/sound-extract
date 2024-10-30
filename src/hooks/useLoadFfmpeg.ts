aba88c8160b4d05f463322a4f094dd0bc0a039bb
coderduan <1961018000@qq.com>
2024-10-30 15:26:58 +0800

    refactor: 仅使用单线程处理

--- src/hooks/useLoadFfmpeg.ts
+++ src/hooks/useLoadFfmpeg.ts
@@ -3,32 +3,10 @@ import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { toBlobURL } from "@ffmpeg/util";
 
 /**
- * 检查 Worker 和 SharedArrayBuffer 支持
- */
-const checkEnvironmentSupport = () => {
-  try {
-    return {
-      hasWorker: typeof Worker !== 'undefined',
-      hasSharedArrayBuffer: typeof SharedArrayBuffer !== 'undefined'
-    };
-  } catch {
-    return {
-      hasWorker: false,
-      hasSharedArrayBuffer: false
-    };
-  }
-};
-
-const { hasWorker, hasSharedArrayBuffer } = checkEnvironmentSupport();
-const isMultiThreadSupported = hasWorker && hasSharedArrayBuffer;
-
-/**
  * cdn地址
  * 知乎的unpkg
  */
-const BASE_URL = isMultiThreadSupported
-  ? "https://unpkg.zhihu.com/@ffmpeg/core-mt@0.12.6/dist/esm"
-  : "https://unpkg.zhihu.com/@ffmpeg/core@0.12.6/dist/esm";
+const BASE_URL = "https://unpkg.zhihu.com/@ffmpeg/core@0.12.6/dist/esm";
 
 export const useLoadFfmpeg = () => {
   const ffmpegRef = useRef<FFmpeg>(new FFmpeg());
@@ -48,9 +26,6 @@ export const useLoadFfmpeg = () => {
         `${BASE_URL}/ffmpeg-core.wasm`,
         "application/wasm",
       ),
-      ...(isMultiThreadSupported && {
-        workerURL: await toBlobURL(`${BASE_URL}/ffmpeg-core.worker.js`, 'text/javascript'),
-      }),
     });
 
     setLoading(false);
