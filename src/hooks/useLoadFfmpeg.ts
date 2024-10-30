1ecc18f8489f3120bb35860195bd0d9f2b24e248
coderduan <1961018000@qq.com>
2024-10-30 15:07:30 +0800

    refactor: 兼容单线程版本
    自动检测环境是否支持 SharedArrayBuffer，如果不支持就会使用单线程版本的 ffmpeg

--- src/hooks/useLoadFfmpeg.ts
+++ src/hooks/useLoadFfmpeg.ts
@@ -3,10 +3,25 @@ import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { toBlobURL } from "@ffmpeg/util";
 
 /**
+ * 检查环境是否支持 SharedArrayBuffer
+ */
+const checkSharedArrayBufferSupport = () => {
+  try {
+    return typeof SharedArrayBuffer !== 'undefined';
+  } catch {
+    return false;
+  }
+};
+
+const hasSAB = checkSharedArrayBufferSupport();
+
+/**
  * cdn地址
  * 知乎的unpkg
  */
-const BASE_URL = "https://unpkg.zhihu.com/@ffmpeg/core-mt@0.12.6/dist/esm";
+const BASE_URL = hasSAB
+  ? "https://unpkg.zhihu.com/@ffmpeg/core-mt@0.12.6/dist/esm"
+  : "https://unpkg.zhihu.com/@ffmpeg/core@0.12.6/dist/esm";
 
 export const useLoadFfmpeg = () => {
   const ffmpegRef = useRef<FFmpeg>(new FFmpeg());
@@ -26,7 +41,9 @@ export const useLoadFfmpeg = () => {
         `${BASE_URL}/ffmpeg-core.wasm`,
         "application/wasm",
       ),
-      workerURL: await toBlobURL(`${BASE_URL}/ffmpeg-core.worker.js`, 'text/javascript'),
+      ...(hasSAB && {
+        workerURL: await toBlobURL(`${BASE_URL}/ffmpeg-core.worker.js`, 'text/javascript'),
+      }),
     });
 
     setLoading(false);
